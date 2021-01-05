9fa20d9f0966326b54a399e6a6f2caa27c341c21
Admin <phuwanat023@rmutsvmail.com>
2021-01-05 17:55:46 +0700

    Finished

--- src/LAB3/ex_if.java
+++ src/LAB3/ex_if.java
@@ -1,16 +1,15 @@
 package LAB3;
 
 public class ex_if {
-    public static void main (String[] args){
-
-        int x = 10, y =20;
-        if (x==y) {
-            System.out.println("True");
-        }else
-        System.out.println("False");
-    }
-
-
+    public static void main(String[] args) {
+        int x = 20,y = 20;
+        if (x==y){
+            System.out.println("true");
+        }else{
+            System.out.println("False.");
+        }
+        System.out.println("Good Bye.");
 
 
-}
+    }//main
+}//class
